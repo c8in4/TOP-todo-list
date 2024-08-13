256b3d3bdf6044b559d71488f9192f78c63cca05
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-08-13 21:34:07 +1200

    Create todoClass.js with constructor

---
+++ src/todoClass.js
@@ -0,0 +1,10 @@
+class Todo {
+  constructor(title, description, dueDate, priority, note, checklist) {
+    this.title = title // string
+    this.description = description // string
+    this.dueDate = dueDate // ??
+    this.priority = priority // 'enum'?
+    this.note = note // string
+    this.checklist = checklist // boolean
+  }
+}
