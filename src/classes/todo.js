57a79f5efc9b56e0a1bcffb3d058bec2a41eb4bb
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-09-09 13:46:08 +1200

    Change order of properties in todo.js

--- src/classes/todo.js
+++ src/classes/todo.js
@@ -3,38 +3,24 @@ import { format } from "date-fns"
 export default class Todo {
   constructor(
     title,
+    priority,
     description,
     dueDate,
-    priority,
     // note,
     // checklist
   ) {
     this.title = title
-    this.description = description
-    this.dueDate = dueDate || format(new Date(), "dd MMM yyyy")
     this.priority = priority
+    this.description = description || "no description"
+    this.dueDate = dueDate || format(new Date(), "dd MMM yyyy")
     // this.note = note
     // this.checklist = checklist
   }
 
-  updateTodo(newTitle, newDescription, newDueDate, newPriority) {
+  updateTodo(newTitle, newPriority, newDueDate, newDescription) {
     this.title = newTitle ? newTitle : this.title
-    this.description = newDescription ? newDescription : this.description
-    this.dueDate = newDueDate ? newDueDate : this.dueDate
     this.priority = newPriority ? newPriority : this.priority
+    this.dueDate = newDueDate ? newDueDate : this.dueDate
+    this.description = newDescription ? newDescription : this.description
   }
 }
-
-////////////////////////////////////////////////////////////////
-// testing:
-
-// console.group('testing of todo')
-// // const firstTodo = new Todo('test title', 'this is a test description', format(new Date(), 'dd MMM yyyy'), 'normal')
-// const firstTodo = new Todo("test title", "this is a test description", "", "normal")
-
-// console.log(firstTodo)
-// firstTodo.updateTodo("new", "new description", "", "")
-
-// console.log(firstTodo)
-
-// console.groupEnd()
