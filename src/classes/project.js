cbf4b71494335942c950fe05a28b2b6c7ad15841
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-09-09 21:46:41 +1200

    Update classes
    todo.js && project.js
    - remove unnecessary method to update instance of the class

--- src/classes/project.js
+++ src/classes/project.js
@@ -4,10 +4,6 @@ export default class Project {
     this.todoList = []
   }
 
-  updateProjectName(newName) {
-    this.name = newName ? newName : this.name
-  }
-
   addTodo(newTodo) {
     this.todoList.push(newTodo)
   }
@@ -16,31 +12,7 @@ export default class Project {
     if (todoIndex < this.todoList.length) {
       this.todoList.splice(todoIndex, 1)
     } else {
-      console.log('invalid index')
+      console.log("invalid index")
     }
   }
 }
-
-////////////////////////////////////////////////////////////////
-// testing
-// import Todo from "./todo"
-
-// console.group("testing of project")
-// const testProject = new Project("New Project")
-// console.log(testProject)
-
-// testProject.updateProjectName("New Project Name")
-// const testTodo = new Todo("todo")
-
-// testProject.addTodo(testTodo)
-// testProject.addTodo(testTodo)
-// testProject.addTodo(testTodo)
-
-// console.log(testProject)
-
-// testProject.removeTodo(0)
-// testProject.removeTodo(2)
-
-// console.log(testProject)
-
-// console.groupEnd()
--- src/classes/todo.js
+++ src/classes/todo.js
@@ -1,26 +1,17 @@
-import { format } from "date-fns"
-
 export default class Todo {
   constructor(
     title,
     priority,
-    description,
     dueDate,
+    description,
     // note,
     // checklist
   ) {
     this.title = title
     this.priority = priority
+    this.dueDate = dueDate
     this.description = description || "no description"
-    this.dueDate = dueDate || format(new Date(), "dd MMM yyyy")
     // this.note = note
     // this.checklist = checklist
-  }
-
-  updateTodo(newTitle, newPriority, newDueDate, newDescription) {
-    this.title = newTitle ? newTitle : this.title
-    this.priority = newPriority ? newPriority : this.priority
-    this.dueDate = newDueDate ? newDueDate : this.dueDate
-    this.description = newDescription ? newDescription : this.description
   }
 }
