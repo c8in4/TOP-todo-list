deaf8fb92fa55569990b215f6868d3abffae1c57
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-09-09 13:48:04 +1200

    Update modalCreator.js
    Add createFormButtons()
    - render dialog buttons instead of hard-coded into index.html
    
    Add createSelectInput()
    - for todo dialog to select a todo's priority
    
    Add createDateInput()
    - for todo dialog to select a due date
    
    Update createTodoDialog()
    - make use of createSelectInput() and createDateInput()

--- src/modalCreator.js
+++ src/modalCreator.js
@@ -1,35 +1,95 @@
-export const dialog = document.querySelector("#dialog")
-export const dialogHeader = document.querySelector("#dialogHeader")
-export const dialogFormContent = document.querySelector("#dialogFormContent")
+import { format } from "date-fns"
 
-dialog.addEventListener("close", () => {
-  dialogHeader.textContent = ""
-  dialogFormContent.innerHTML = ""
-})
+const dialogHeader = document.querySelector("#dialogHeader")
+const dialogFormContent = document.querySelector("#dialogFormContent")
+const dialogFormButtons = document.querySelector(".dialogFormButtons")
 
-export const CreateProjectDialog = (headerText) => {
+export function createProjectDialog(headerText) {
+  resetDialog()
+
   dialogHeader.textContent = headerText
   const projectNameInput = createTextInput("Project Name:", "projectName")
+
   dialogFormContent.append(projectNameInput)
+  createFormButtons()
 }
 
-export const CreateTodoDialog = (headerText) => {
+export function createTodoDialog(headerText) {
+  resetDialog()
+
   dialogHeader.textContent = headerText
   const todoTitleInput = createTextInput("Todo Title:", "todoTitle")
 
+  const groupDiv = document.createElement("div")
+  const todoPriorityInput = createSelectInput("Priority:", "todoPriority")
+  const todoDueDate = createDateInput("Due Date:", "todoDueDate")
+  groupDiv.append(todoPriorityInput, todoDueDate)
+
   const todoNoteTextarea = createTextarea("Note:", "todoNote")
 
-  dialogFormContent.append(todoTitleInput, todoNoteTextarea)
+  dialogFormContent.append(todoTitleInput, groupDiv, todoNoteTextarea)
+  createFormButtons()
+}
+
+function resetDialog() {
+  dialogHeader.textContent = ""
+  dialogFormContent.innerHTML = ""
+  dialogFormButtons.innerHTML = ""
 }
 
 function createTextInput(label, id) {
   const inputLabel = document.createElement("label")
   const labelText = document.createElement("p")
-
   inputLabel.for = id
   labelText.textContent = label
+
   const input = document.createElement("input")
   input.id = id
+
+  inputLabel.append(labelText, input)
+  return inputLabel
+}
+
+function createSelectInput(label, id) {
+  const inputLabel = document.createElement("label")
+  const labelText = document.createElement("p")
+  inputLabel.for = id
+  labelText.textContent = label
+
+  const input = document.createElement("select")
+  input.name = id
+  input.id = id
+
+  function createOption(value, text) {
+    const option = document.createElement("option")
+    option.name = value
+    option.value = value
+    option.textContent = text
+
+    return option
+  }
+
+  const optionNormal = createOption("normal", "Normal")
+  const optionLow = createOption("low", "Low")
+  const optionHigh = createOption("high", "High")
+
+  input.append(optionLow, optionNormal, optionHigh)
+
+  inputLabel.append(labelText, input)
+  return inputLabel
+}
+
+function createDateInput(label, id) {
+  const inputLabel = document.createElement("label")
+  const labelText = document.createElement("p")
+  inputLabel.for = id
+  labelText.textContent = label
+
+  const input = document.createElement("input")
+  input.type = "date"
+  input.value = format(new Date(), "yyyy-MM-dd")
+  input.id = id
+
   inputLabel.append(labelText, input)
   return inputLabel
 }
@@ -44,6 +104,21 @@ function createTextarea(label, id) {
   textarea.id = id
   textarea.cols = 45
   textarea.rows = 5
+
   inputLabel.append(labelText, textarea)
   return inputLabel
+}
+
+function createFormButtons() {
+  const saveFormButton = document.createElement("button")
+  saveFormButton.id = "saveFormButton"
+  saveFormButton.value = "default"
+  saveFormButton.textContent = "Save"
+
+  const cancelFormButton = document.createElement("button")
+  cancelFormButton.value = "cancelFormButton"
+  cancelFormButton.formMethod = "dialog"
+  cancelFormButton.textContent = "Cancel"
+
+  dialogFormButtons.append(saveFormButton, cancelFormButton)
 }
