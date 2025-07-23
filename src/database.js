e8824f5b262b88aea8aaeaf54cb586d86ae1d010
c8in4 <106409156+c8in4@users.noreply.github.com>
2025-07-23 23:18:58 +1200

    Update database.js to use of localStorageIO.js
    Create a function that initiates the database by checking if there is
    data saved in local storage. It then populates the database with that
    data or creates a new data with a default project with an example todo.

--- src/database.js
+++ src/database.js
@@ -1,25 +1,24 @@
 import ListOfProjects from "./classes/listOfProjects"
+import { getData } from "./localStorageIO"
 import createDefaultProject from "./createDefaultProject"
-import testProjectCreator from "./createTestProjects"
 
-export let database = loadProjects()
+export let database = initDatabase()
 
-export function saveProjects() {
-  const projectJson = JSON.stringify(database)
-  localStorage.setItem("projects", projectJson)
-  console.info("Projects saved to local storage")
+function initDatabase() {
+  const newDatabase = new ListOfProjects
+  if (getData('projects')) {
+    getProjectsFromLocalStorage(newDatabase)
+  } else {
+    newDatabase.addProject(createDefaultProject())
+  }
+  return newDatabase
 }
 
-export function loadProjects() {
-  const projects = localStorage.getItem("projects")
-  const parsedProjects = JSON.parse(projects)
-  if (projects) {
-    console.info("Loaded projects from local storage")
-    return parsedProjects
-  } else {
-    console.info("No local projects found. Created default project with example todo")
-    const newDatabase = new ListOfProjects
-    newDatabase.addProject(createDefaultProject())
-    return newDatabase
+function getProjectsFromLocalStorage(database) {
+  if (getData('projects')) {
+    const savedProjects = JSON.parse(localStorage.getItem('projects'))
+    savedProjects.projects.forEach(project => {
+      database.addProject(project)
+    });
   }
 }
