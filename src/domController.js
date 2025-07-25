5563defffb7e449dd72b501ca71809c0bd9ca47a
c8in4 <106409156+c8in4@users.noreply.github.com>
2025-07-26 09:18:35 +1200

    Add renderProjects() to domController.js

--- src/domController.js
+++ src/domController.js
@@ -1,185 +1,223 @@
-import { getListOfProjects } from "./projectsDatabase"
-import renderSidebar from "./renderSidebar"
-import renderMain from "./renderMain"
-import Project from "./classes/project"
-import Todo from "./classes/todo"
-import { createProjectDialog, createTodoDialog } from "./modalCreator"
+// import renderSidebar from "./renderSidebar"
+// import renderMain from "./renderMain"
+// import Project from "./classes/project"
+// import Todo from "./classes/todo"
+// import { createProjectDialog, createTodoDialog } from "./modalCreator"
 
-export function render() {
-  renderSidebar(activeProjectIndex)
-  renderMain(getListOfProjects()[activeProjectIndex])
-  setupEventListeners()
+export function renderProjects(projects) {
+  const mainContainer = document.querySelector('main')
+  projects.forEach(project => {
+    mainContainer.appendChild(createProjectContainer(project))
+  });
 }
 
-const dialog = document.querySelector("#dialog")
-
-let activeProjectIndex = 0
-
-/* this next 2 elements don't get re-rendered, they only need the eventListener
-setup once
-*/
-;(function setupNewProjectEventListener() {
-  const newProjectButton = document.querySelector("#createProject")
-  newProjectButton.addEventListener("click", () => {
-    newProjectEvent()
-  })
-})()
-//
-;(function setupProjectListEventListener() {
-  const unorderedListOfProjects = document.querySelector("#listOfProjects")
-  unorderedListOfProjects.addEventListener("click", (e) => {
-    const index = e.target.dataset.index
-    if (index) activeProjectIndex = index
-    render()
-  })
-})()
-
-function setupEventListeners() {
-  ;(function newTodo() {
-    const newTodoButton = document.querySelector("#createTodo")
-    if (newTodoButton) {
-      newTodoButton.addEventListener("click", () => {
-        newTodoEvent()
-      })
-    }
-  })()
-  //
-  ;(function editProject() {
-    const editProjectButton = document.querySelector(
-      ".headerContainer .editButton",
-    )
-    if (editProjectButton) {
-      editProjectButton.addEventListener("click", () => {
-        editProjectEvent()
-      })
-    }
-  })()
-  //
-  ;(function deleteProject() {
-    const deleteProjectButton = document.querySelector(
-      ".headerContainer .deleteButton",
-    )
-    if (deleteProjectButton) {
-      deleteProjectButton.addEventListener("click", () => {
-        if (confirm("Are you sure you want to delete this?")) {
-          getListOfProjects().splice(activeProjectIndex, 1)
-          activeProjectIndex--
-          if (activeProjectIndex < 0) activeProjectIndex = 0
-          render()
-        }
-      })
-    }
-  })()
-  //
-  ;(function editTodo() {
-    const allEditTodoButtons = document.querySelectorAll(
-      "#todoContainer .editButton",
-    )
-    allEditTodoButtons.forEach((editButton, index) => {
-      editButton.addEventListener("click", () => {
-        editTodoEvent(index)
-      })
-    })
-  })()
-  //
-  ;(function deleteTodo() {
-    const allDeleteTodoButtons = document.querySelectorAll(
-      "#todoContainer .deleteButton",
-    )
-    allDeleteTodoButtons.forEach((deleteButton, index) => {
-      deleteButton.addEventListener("click", () => {
-        if (confirm("Are you sure you want to delete this?")) {
-          const activeProject = getListOfProjects()[activeProjectIndex]
-          activeProject.todoList.splice(index, 1)
-          render()
-        }
-      })
-    })
-  })()
+function createProjectContainer(project) {
+  const container = document.createElement('div')
+  const header = document.createElement('h2')
+  header.innerText = project.name
+  const todos = createTodoContainer(project.todoList)
+  container.append(header, todos)
+  return container
 }
 
-function newProjectEvent() {
-  console.info("new project button pressed")
-  createProjectDialog("Create new Project")
-  const projectName = document.querySelector("#projectName")
-  projectName.focus()
-  setupSaveButton(() => {
-    const newProject = new Project(projectName.value)
-    if (!newProject.name) return
-    getListOfProjects().push(newProject)
-    activeProjectIndex = getListOfProjects().length - 1
-    render()
-  })
-  dialog.showModal()
+function createTodoContainer(todoList) {
+  const todosContainer = document.createElement('div')
+  todoList.forEach(todo => {
+    todosContainer.appendChild(createTodo(todo))
+  });
+  return todosContainer
 }
 
-function newTodoEvent() {
-  console.info("new todo button pressed")
-  createTodoDialog("Create new Todo")
-  const todoTitle = document.querySelector("#todoTitle")
-  const todoPriority = document.querySelector("#todoPriority")
-  const todoDueDate = document.querySelector("#todoDueDate")
-  const todoDescription = document.querySelector("#todoDescription")
-  setupSaveButton(() => {
-    const newTodo = new Todo(
-      todoTitle.value,
-      todoPriority.value,
-      todoDueDate.value,
-      todoDescription.value,
-    )
-    if (!newTodo.title) return
-    const activeProject = getListOfProjects()[activeProjectIndex]
-    activeProject.todoList.push(newTodo)
-    render()
-  })
-  dialog.showModal()
+function createTodo(todo) {
+  const todoContainer = document.createElement('div')
+  const title = document.createElement('h3')
+  title.innerText = todo.title
+  const description = document.createElement('p')
+  description.innerText = todo.description
+  const dueDate = document.createElement('p')
+  dueDate.innerText = 'due: ' + todo.dueDate
+  const priority = document.createElement('p')
+  priority.innerText = 'priority: ' + todo.priority
+
+  todoContainer.append(title, dueDate, priority, description)
+  return todoContainer
 }
 
-function editProjectEvent() {
-  console.info("edit project button pressed")
-  createProjectDialog("Edit Project")
-  const projectName = document.querySelector("#projectName")
-  const activeProject = getListOfProjects()[activeProjectIndex]
-  projectName.value = activeProject.name
-  projectName.focus()
-  setupSaveButton(() => {
-    activeProject.name = projectName.value
-    render()
-  })
-  dialog.showModal()
-}
+// export function render() {
+//   renderSidebar(activeProjectIndex)
+//   renderMain(getListOfProjects()[activeProjectIndex])
+//   setupEventListeners()
+// }
 
-function editTodoEvent(todoIndex) {
-  console.info("edit todo button pressed")
-  createTodoDialog("Edit Todo")
-  const todoTitle = document.querySelector("#todoTitle")
-  const todoPriority = document.querySelector("#todoPriority")
-  const todoDueDate = document.querySelector("#todoDueDate")
-  const todoDescription = document.querySelector("#todoDescription")
+// const dialog = document.querySelector("#dialog")
 
-  const activeProject = getListOfProjects()[activeProjectIndex]
-  const activeTodo = activeProject.todoList[todoIndex]
+// let activeProjectIndex = 0
 
-  todoTitle.value = activeTodo.title
-  todoPriority.value = activeTodo.priority
-  todoDueDate.value = activeTodo.dueDate
-  todoDescription.value = activeTodo.description
+// /* this next 2 elements don't get re-rendered, they only need the eventListener
+// setup once
+// */
+// ;(function setupNewProjectEventListener() {
+//   const newProjectButton = document.querySelector("#createProject")
+//   newProjectButton.addEventListener("click", () => {
+//     newProjectEvent()
+//   })
+// })()
+// //
+// ;(function setupProjectListEventListener() {
+//   const unorderedListOfProjects = document.querySelector("#listOfProjects")
+//   unorderedListOfProjects.addEventListener("click", (e) => {
+//     const index = e.target.dataset.index
+//     if (index) activeProjectIndex = index
+//     render()
+//   })
+// })()
 
-  setupSaveButton(() => {
-    activeTodo.title = todoTitle.value
-    activeTodo.priority = todoPriority.value
-    activeTodo.dueDate = todoDueDate.value
-    activeTodo.description = todoDescription.value
-    render()
-  })
-  dialog.showModal()
-}
+// function setupEventListeners() {
+//   ;(function newTodo() {
+//     const newTodoButton = document.querySelector("#createTodo")
+//     if (newTodoButton) {
+//       newTodoButton.addEventListener("click", () => {
+//         newTodoEvent()
+//       })
+//     }
+//   })()
+//   //
+//   ;(function editProject() {
+//     const editProjectButton = document.querySelector(
+//       ".headerContainer .editButton",
+//     )
+//     if (editProjectButton) {
+//       editProjectButton.addEventListener("click", () => {
+//         editProjectEvent()
+//       })
+//     }
+//   })()
+//   //
+//   ;(function deleteProject() {
+//     const deleteProjectButton = document.querySelector(
+//       ".headerContainer .deleteButton",
+//     )
+//     if (deleteProjectButton) {
+//       deleteProjectButton.addEventListener("click", () => {
+//         if (confirm("Are you sure you want to delete this?")) {
+//           getListOfProjects().splice(activeProjectIndex, 1)
+//           activeProjectIndex--
+//           if (activeProjectIndex < 0) activeProjectIndex = 0
+//           render()
+//         }
+//       })
+//     }
+//   })()
+//   //
+//   ;(function editTodo() {
+//     const allEditTodoButtons = document.querySelectorAll(
+//       "#todoContainer .editButton",
+//     )
+//     allEditTodoButtons.forEach((editButton, index) => {
+//       editButton.addEventListener("click", () => {
+//         editTodoEvent(index)
+//       })
+//     })
+//   })()
+//   //
+//   ;(function deleteTodo() {
+//     const allDeleteTodoButtons = document.querySelectorAll(
+//       "#todoContainer .deleteButton",
+//     )
+//     allDeleteTodoButtons.forEach((deleteButton, index) => {
+//       deleteButton.addEventListener("click", () => {
+//         if (confirm("Are you sure you want to delete this?")) {
+//           const activeProject = getListOfProjects()[activeProjectIndex]
+//           activeProject.todoList.splice(index, 1)
+//           render()
+//         }
+//       })
+//     })
+//   })()
+// }
 
-function setupSaveButton(saveFunction) {
-  const saveFormButton = document.querySelector("#saveFormButton")
-  saveFormButton.addEventListener("click", (e) => {
-    e.preventDefault()
-    dialog.close()
-    saveFunction()
-  })
-}
+// function newProjectEvent() {
+//   console.info("new project button pressed")
+//   createProjectDialog("Create new Project")
+//   const projectName = document.querySelector("#projectName")
+//   projectName.focus()
+//   setupSaveButton(() => {
+//     const newProject = new Project(projectName.value)
+//     if (!newProject.name) return
+//     getListOfProjects().push(newProject)
+//     activeProjectIndex = getListOfProjects().length - 1
+//     render()
+//   })
+//   dialog.showModal()
+// }
+
+// function newTodoEvent() {
+//   console.info("new todo button pressed")
+//   createTodoDialog("Create new Todo")
+//   const todoTitle = document.querySelector("#todoTitle")
+//   const todoPriority = document.querySelector("#todoPriority")
+//   const todoDueDate = document.querySelector("#todoDueDate")
+//   const todoDescription = document.querySelector("#todoDescription")
+//   setupSaveButton(() => {
+//     const newTodo = new Todo(
+//       todoTitle.value,
+//       todoPriority.value,
+//       todoDueDate.value,
+//       todoDescription.value,
+//     )
+//     if (!newTodo.title) return
+//     const activeProject = getListOfProjects()[activeProjectIndex]
+//     activeProject.todoList.push(newTodo)
+//     render()
+//   })
+//   dialog.showModal()
+// }
+
+// function editProjectEvent() {
+//   console.info("edit project button pressed")
+//   createProjectDialog("Edit Project")
+//   const projectName = document.querySelector("#projectName")
+//   const activeProject = getListOfProjects()[activeProjectIndex]
+//   projectName.value = activeProject.name
+//   projectName.focus()
+//   setupSaveButton(() => {
+//     activeProject.name = projectName.value
+//     render()
+//   })
+//   dialog.showModal()
+// }
+
+// function editTodoEvent(todoIndex) {
+//   console.info("edit todo button pressed")
+//   createTodoDialog("Edit Todo")
+//   const todoTitle = document.querySelector("#todoTitle")
+//   const todoPriority = document.querySelector("#todoPriority")
+//   const todoDueDate = document.querySelector("#todoDueDate")
+//   const todoDescription = document.querySelector("#todoDescription")
+
+//   const activeProject = getListOfProjects()[activeProjectIndex]
+//   const activeTodo = activeProject.todoList[todoIndex]
+
+//   todoTitle.value = activeTodo.title
+//   todoPriority.value = activeTodo.priority
+//   todoDueDate.value = activeTodo.dueDate
+//   todoDescription.value = activeTodo.description
+
+//   setupSaveButton(() => {
+//     activeTodo.title = todoTitle.value
+//     activeTodo.priority = todoPriority.value
+//     activeTodo.dueDate = todoDueDate.value
+//     activeTodo.description = todoDescription.value
+//     render()
+//   })
+//   dialog.showModal()
+// }
+
+// function setupSaveButton(saveFunction) {
+//   const saveFormButton = document.querySelector("#saveFormButton")
+//   saveFormButton.addEventListener("click", (e) => {
+//     e.preventDefault()
+//     dialog.close()
+//     saveFunction()
+//   })
+// }
